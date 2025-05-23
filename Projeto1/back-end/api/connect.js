3bbf9d7d083f200333921daabf8c58ee031b0450
Arthur Vinicius <contatoviniy001@gmail.com>
2025-05-23 12:45:02 -0300

    connect.js

--- Projeto1/back-end/api/connect.js
+++ Projeto1/back-end/api/connect.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const URL =
-  "mongodb+srv://Arth:GkpFux4vjl1GzSqE@cluster0.froclnc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "SUA URL DO MONGODB";
 
 const client = new MongoClient(URL);
 export const db = client.db("spotifyAula");
